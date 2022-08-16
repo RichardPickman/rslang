542d92d2c06019b946951910df34fed3862066cb
Natallia Kostsina <natakostina123@gmail.com>
2022-08-16 17:30:26 +0200

    feat: add basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { RouteNames } from './router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles.css'
+import Homepage from './pages/Homepage'
+import Login from './pages/Login';
+import Textbook from './pages/Textbook';
+import Games from './pages/Games';
+import Statistics from './pages/Statistics';
+import NotFound from './pages/NotFound/index';
 
 export const App = () => {
   return (
-    <>
-      <h1>App!!!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={RouteNames.HOMEPAGE} element={<Homepage />} />
+        <Route path={RouteNames.LOGIN} element={<Login />} />
+        <Route path={RouteNames.TEXTBOOK} element={<Textbook />} />
+        <Route path={RouteNames.GAMES} element={<Games />} />
+        <Route path={RouteNames.STATISTICS} element={<Statistics />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
