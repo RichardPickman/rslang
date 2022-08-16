323c68b3afb6d92d4277dc350a44edc84c0e0ffe
Natallia Kostsina <natakostina123@gmail.com>
2022-08-16 20:10:14 +0200

    fix: replace with semantic tags

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from '../Navigation';
+import './styles.scss';
 
 const Header = () => {
   return (
-    <div>
+    <header className='header'>
       <Navigation />
-    </div>
+    </header>
   );
 };
 
