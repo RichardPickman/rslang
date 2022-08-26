37d87b19f1c25413eef4d3b6b6805e121c96362d
RichardPickman <drezzerock@gmail.com>
2022-08-26 16:40:45 +0200

    feat: add error handling

--- src/utils/checkAuthentication.ts
+++ src/utils/checkAuthentication.ts
@@ -2,7 +2,6 @@ import { TypedRequest, TypedResponse } from './../Types/index';
 import { NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import settings from '../common/config';
-import errors from '../Errors/appErrors';
 
 const ALLOWED_PATHS = ['/signin', '/signup'];
 const DOC_PATH_REGEX = /^\/doc\/?$/;
@@ -34,22 +33,26 @@ const checkAuthentication = (
 
   const rawToken = req.headers.authorization;
   if (!rawToken) {
-    throw new errors.AUTHORIZATION_ERROR();
+    return res.sendStatus(401);
   }
 
   try {
     const token = rawToken.slice(7, rawToken.length);
     const secret = req.path.includes('tokens')
-      ? (settings.JWT_REFRESH_SECRET_KEY as string)
-      : (settings.JWT_SECRET_KEY as string);
-    const { id, tokenId } = jwt.verify(token, secret) as JwtPayload;
+      ? (settings.JWT_REFRESH_SECRET_KEY as jwt.Secret)
+      : (settings.JWT_SECRET_KEY as jwt.Secret);
+
+    const { id, tokenId } = jwt.verify(token, secret) as jwt.JwtPayload;
+
     req.userId = id;
     req.tokenId = tokenId;
-  } catch (error) {
-    throw new errors.AUTHORIZATION_ERROR();
-  }
 
-  next();
+    next();
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.status(401).send('Incorrect refresh token');
+    }
+  }
 };
 
 export default checkAuthentication;
