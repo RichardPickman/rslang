8ea76059c3f6a56bdea55c0b15de71ee055197ff
RichardPickman <drezzerock@gmail.com>
2022-08-26 16:42:01 +0200

    refactor: move authentication and morgan middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,13 @@
 import cors from 'cors';
 import helmet from 'helmet';
-import morgan from 'morgan';
 import express from 'express';
-import { stream } from './common/logging';
 import router from './Routes';
-
-import checkAuthentication from './utils/checkAuthentication';
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
-app.use(checkAuthentication);
-
-app.use(
-  morgan(
-    ':method :status :url :userId size req :req[content-length] res :res[content-length] - :response-time ms',
-    {
-      stream: stream
-    }
-  )
-);
 
 app.use(router);
 
