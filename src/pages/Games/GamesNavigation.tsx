793280f53b73f9d2952fb888994e86779bc833e3
Natallia Kostsina <natakostina123@gmail.com>
2022-09-04 23:36:38 +0200

    feat: add styles for games page and sprint game

--- src/pages/Games/GamesNavigation.tsx
+++ src/pages/Games/GamesNavigation.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Section from '../../components/Section';
 import { Games } from '../../data/constants';
-import { RouteNames } from '../../router';
 import { GameMode } from '../../types/types';
 import styles from './styles.module.scss';
+import { motion } from 'framer-motion';
+import PatternStars from '../../assets/img/patterns/img-stars.png';
 
 const GamesNavigation = ({ state }: { state: GameMode }) => {
   return (
-    <ul className={`${styles['nav']}`}>
-      {Games.map((game) => {
-        return <li className={`${styles['nav__item']}`} key={game.id}>
-          <p className={`${styles['game__title']}`}>{game.title}</p>
-          <p className={`${styles['game__description']}`}>{game.description}</p>
-          <Link
-            to={`${RouteNames.GAMES}/${RouteNames.SPRINT_GAME}`}
-            state={state}
-          >
-            Поехали!
-          </Link>
-        </li>
-      })}
-
-    </ul>
+    <div className={`${styles['section-games']}`}>
+      <motion.div
+        className={`${styles['bg-items']}`}
+        animate={{ rotate: 7 }}
+        transition={{ repeat: Infinity, duration: 4, repeatType: 'mirror' }}
+      />
+      <img  className={`${styles['pattern-stars']}`} src={PatternStars} />
+      <Section title={'Выбери игру'}>
+        <>
+          <ul className={`${styles['nav']}`}>
+            {Games.map((game) => {
+              return <li className={`${styles['nav__item']}`} key={game.id}>
+                <p className={`${styles['game__title']}`}>{game.title}</p>
+                <p className={`${styles['game__description']}`}>{game.description}</p>
+                <Link
+                  to={game.link}
+                  state={state}
+                >
+                  Поехали!
+                </Link>
+              </li>
+            })}
+          </ul>
+        </>
+      </Section>
+    </div>
   );
 };
 
--- src/pages/Games/SprintGame/OnStartPage.tsx
+++ src/pages/Games/SprintGame/OnStartPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Games } from '../../../data/constants';
 import { useActions } from '../../../hooks/useActions';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 import { GamePhase } from '../../../types/types';
@@ -9,18 +10,20 @@ const OnStartPage = () => {
   const { isAuth, user } = useAppSelector((state) => state.auth);
   const { setPhaseAction, setGameWordsAction, setFetchedWordsAction } = useActions();
   const onBtnClick = () => {
-    generateRandomWordsFromPage({unit, page, isAuth, user, setFetchedWordsAction})
-    .then((pairs) => {
-      setGameWordsAction(pairs);
-      setPhaseAction(GamePhase.STARTED);
-    })
-    .catch((error) => console.log(error));
+    generateRandomWordsFromPage({ unit, page, isAuth, user, setFetchedWordsAction })
+      .then((pairs) => {
+        setGameWordsAction(pairs);
+        setPhaseAction(GamePhase.STARTED);
+      })
+      .catch((error) => error);
   }
 
   return (
-    <div>
-      <p>Правила игры бла-бла-бла</p>
-      <button type='button' onClick={onBtnClick}>Поехали!</button>
+    <div className={'on-start'}>
+      <div className={'rules'}>
+        <p>{Games.find((game) => game.id === 1)?.rules}</p>
+        <button className={`${'btn'} ${'btn_start'}`} type='button' onClick={onBtnClick}>Поехали!</button>
+      </div>
     </div>
   );
 };
