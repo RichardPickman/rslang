5a6f38e523215ea82cc6dc6582973620b233feef
alex <email@damain.com>
2022-09-05 13:06:22 +0300

    feat: add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Container from '../Container';
 import Navigation from '../Navigation';
 import styles from './styles.module.scss';
+import logo_full from '../../assets/img/logo/logorslang.png'
+import logo from '../../assets/img/logo/logo.png'
 
 const Header = () => {
   return (
     <header className={styles.header}>
+      <div className={styles['logo-box']}>
+        <img  className={styles['logo-full']} src={logo_full} alt=""></img>
+        <img  className={styles['logo']} src={logo} alt=""></img>
+      </div>
       <Container>
         <div className={styles.header__inner}>
           <Navigation />
