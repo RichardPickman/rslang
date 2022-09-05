b955501e909c85fb61f54f24d61d7a75b7410727
alex <email@damain.com>
2022-09-05 13:22:59 +0300

    feat: homepage: add basic responsive

--- src/components/forHomepage/Promo/index.tsx
+++ src/components/forHomepage/Promo/index.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <Container>
         <div className={styles.wrapper}>
           <div style={{ margin: "12rem 0 3rem" }}>
-            <ColorSubTitle width='120' height='40' color='#fff'>
+            <ColorSubTitle width='120' height='40' color='#1ab9ff'>
               Почему Выбирают RS Lang
             </ColorSubTitle>
             <div style={{ margin: "2rem" }}></div>
